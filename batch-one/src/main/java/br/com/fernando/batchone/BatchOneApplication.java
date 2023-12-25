c66650e9b89e54491f125bacf41c76f8c2651f86
Luis Fernando <luisfborges2002@gmail.com>
2023-12-25 16:06:43 -0300

    :sparkles: Initial commit

---
+++ batch-one/src/main/java/br/com/fernando/batchone/BatchOneApplication.java
@@ -0,0 +1,13 @@
+package br.com.fernando.batchone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchOneApplication.class, args);
+	}
+
+}
---
+++ batch-one/src/test/java/br/com/fernando/batchone/BatchOneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fernando.batchone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
